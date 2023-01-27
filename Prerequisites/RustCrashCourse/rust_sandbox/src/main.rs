a14069b20b98fdeefc0e9d63ed7167793b4958ad
Didrick-Manahan <63126473+Didrick-Manahan@users.noreply.github.com>
2023-01-27 01:40:09 -0500

    functions and pointer_refs

--- Prerequisites/RustCrashCourse/rust_sandbox/src/main.rs
+++ Prerequisites/RustCrashCourse/rust_sandbox/src/main.rs
@@ -6,7 +6,9 @@
 //mod arrays;
 //mod vectors;
 //mod conditionals;
-mod loops;
+//mod loops;
+//mod functions;
+mod pointer_ref;
 fn main() {
     //print::run();
     //vars::run();
@@ -16,5 +18,7 @@ fn main() {
     //arrays::run();
     //vectors::run();
     //conditionals::run();
-    loops::run();
+    //loops::run();
+    //functions::run();
+    pointer_ref::run();
 }
