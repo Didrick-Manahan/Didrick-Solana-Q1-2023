ef6ee9ca6dee3da0bf842847fda3414b5973c3fc
Didrick-Manahan <63126473+Didrick-Manahan@users.noreply.github.com>
2023-01-27 22:11:40 -0500

    initial process exchange fn

--- Prerequisites/PaulXEscrow/src/error.rs
+++ Prerequisites/PaulXEscrow/src/error.rs
@@ -12,6 +12,14 @@ pub enum EscrowError {
     /// Not Rent Exempt
     #[error("Not Rent Exempt")]
     NotRentExempt,
+
+    /// Expected Amount Mismatch
+    #[error("Expected Amount Mismatch")]
+    ExpectedAmountMismatch,
+
+    /// Amount Overflow
+    #[error("Amount Overflow")]
+    AmountOverflow,
 }
 
 //implementing a generic trait - "From" trait. The reason we do this conversion is that the entrypoint returns a Result of either nothing or a ProgramError.
