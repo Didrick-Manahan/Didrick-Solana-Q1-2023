b443eeb219c2242faf5dc26002ea1608f77c562c
Didrick-Manahan <63126473+Didrick-Manahan@users.noreply.github.com>
2023-01-27 21:11:45 -0500

    pass ownership to PDA

--- Prerequisites/PaulXEscrow/src/entrypoint.rs
+++ Prerequisites/PaulXEscrow/src/entrypoint.rs
@@ -2,20 +2,16 @@
 
 //smart contracts on Solana are called "Programs"
 use solana_program::{
-    account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, msg, pubkey::Pubkey,
-}; //required crates
+    account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, pubkey::Pubkey,
+};
 
-entrypoint!(process_instruction); //all accounts read or written to must be passed into entrypoint (runtime will parallelize transactions)
+use crate::processor::Processor;
+
+entrypoint!(process_instruction);
 fn process_instruction(
-    program_id: &Pubkey,      //program ID of currently executing program
-    accounts: &[AccountInfo], //used to store state, programs are stateless. Accounts are owned by programs! Only account owner may debit, anyone else can credit!
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
     instruction_data: &[u8],
 ) -> ProgramResult {
-    msg!(
-        "process_instruction: {}: {} accounts, data={:?}",
-        program_id,
-        accounts.len(),
-        instruction_data
-    );
-    Ok(())
+    Processor::process(program_id, accounts, instruction_data)
 }
--- Prerequisites/PaulXEscrow/src/processor.rs
+++ Prerequisites/PaulXEscrow/src/processor.rs
@@ -4,6 +4,7 @@ use solana_program::{
     account_info::{next_account_info, AccountInfo},
     entrypoint::ProgramResult,
     msg,
+    program::invoke,
     program_error::ProgramError,
     program_pack::{IsInitialized, Pack},
     pubkey::Pubkey,
@@ -59,10 +60,51 @@ impl Processor {
             return Err(EscrowError::NotRentExempt.into());
         }
 
+        //Created the escrow struct instance and check it is uninitialized.
         let mut escrow_info = Escrow::unpack_unchecked(&escrow_account.try_borrow_data()?)?; //unpack_unchecked -> function inside state.rs. We never defined it but traits can have default functions that may be overridden but don't have to be!
         if escrow_info.is_initialized() {
             return Err(ProgramError::AccountAlreadyInitialized);
         }
+
+        //Time to populate the struct's fields!
+        escrow_info.is_initialized = true;
+        escrow_info.initializer_pubkey = *initializer.key;
+        escrow_info.temp_token_account_pubkey = *temp_token_account.key;
+        escrow_info.initializer_token_to_receive_account_pubkey = *token_to_receive_account.key;
+        escrow_info.expected_amount = amount;
+
+        //pack is another default function which internally calls our pack_into_slice function.
+        Escrow::pack(escrow_info, &mut escrow_account.try_borrow_mut_data()?)?;
+
+        //Program Derived Addresses do not lie on the ed25519 curve and therefore have no private key associated with them. */
+        let (pda, _bump_seed) = Pubkey::find_program_address(&[b"escrow"], program_id);
+
+        //transfer authority of the temporary token account to the PDA (derived from escrow program)
+        let token_program = next_account_info(account_info_iter)?;
+        let owner_change_ix = spl_token::instruction::set_authority(
+            //token program helper function "set authority"
+            token_program.key,
+            temp_token_account.key,
+            Some(&pda),
+            spl_token::instruction::AuthorityType::AccountOwner,
+            initializer.key,
+            &[&initializer.key],
+        )?;
+
+        //Cross-Program Invocation!
+        msg!("Calling the token program to transfer token account ownership...");
+
+        //program being called through a CPI must be included as an account in the 2nd argument of invoke (and invoke_signed)
+        //check to make sure token program is truly the account of the token program --> spl-token versions above 3.1.1 checks for you!
+        invoke(
+            &owner_change_ix,
+            &[
+                temp_token_account.clone(),
+                initializer.clone(),
+                token_program.clone(),
+            ],
+        )?;
+        //Signature Extension! --> the signature is extended to the CPIs.
 
         Ok(())
     }
